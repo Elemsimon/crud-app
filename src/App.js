6306154cfafb64d15a0c6f8d33861eaa07e5c14e
Elemsimon <elemifesinachisimon@gmail.com>
2022-06-28 19:19:01 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import AllAgendas from './Component/AllAgendas';
+import AddAgenda from './Component/AddAgenda';
+import EditAgenda from './Component/EditAgenda';
+import NavBar from './Component/NavBar';
+import NotFound from './Component/NotFound'; 
+import "./App.css";
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-        Home
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" exact element={<AllAgendas />} />
+        <Route path="/add" element={<AddAgenda />} />
+        <Route path="/edit/:id" element={<EditAgenda />} />
+        <Route element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
