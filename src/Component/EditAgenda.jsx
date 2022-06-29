853dd686c660c79154878f5a19a1456a6720e3ff
Elemsimon <elemifesinachisimon@gmail.com>
2022-06-29 08:02:59 +0100

    update

--- src/Component/EditAgenda.jsx
+++ src/Component/EditAgenda.jsx
@@ -37,7 +37,7 @@ const EditAgenda = () => {
 
     const editAgendaDetails = async() => {
         const response = await editAgenda(id, agenda);
-        navigate('/all');
+        navigate('/');
     }
 
     const onValueChange = (e) => {
